9365a04aa18d3ffac06a43da5dc411943d0a4507
Giuseppe Novielli <novielligiuseppe@gmail.com>
2020-09-05 15:58:12 +0200

    add binding 6.5.6

---
+++ README.md
@@ -0,0 +1,2 @@
+# Xam.IQKeyboardManager 6.5.6
+Xamarin.iOS Binding of hackiftekhar-IQKeyboardManager library https://github.com/hackiftekhar/IQKeyboardManager by Giuseppe Novielli
---
+++ XamIQKeyboardManager.Sample/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace XamIQKeyboardManager.Sample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
